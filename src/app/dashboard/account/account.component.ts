333c91a74b8a991018476e0576b648a1cbb17c77
xSE7EN06 <agao6105@gmail.com>
2025-06-04 13:07:25 -0600

    fix: crear carpeta account en dashboard

---
+++ src/app/dashboard/account/account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account',
+  standalone: false,
+  templateUrl: './account.component.html',
+  styleUrl: './account.component.css'
+})
+export class AccountComponent {
+
+}
