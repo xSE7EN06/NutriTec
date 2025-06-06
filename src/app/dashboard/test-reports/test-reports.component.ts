fb0c2b07ad871dc44e1ce57bae183220a364dd55
xSE7EN06 <agao6105@gmail.com>
2025-06-05 19:14:55 -0600

    style/feat: crear diseño de test-reports, crear tablas y crear datos estaticos para el diseño de las tablas

--- src/app/dashboard/test-reports/test-reports.component.ts
+++ src/app/dashboard/test-reports/test-reports.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ConfirmDeleteDialogComponent } from '../../shared/components/confirm-delete-dialog/confirm-delete-dialog.component';
 
 @Component({
   selector: 'app-test-reports',
@@ -6,6 +9,29 @@ import { Component } from '@angular/core';
   templateUrl: './test-reports.component.html',
   styleUrl: './test-reports.component.css'
 })
-export class TestReportsComponent {
+export class TestReportsComponent{
 
+
+  constructor(private route: Router, private dialog: MatDialog){
+
+  }
+
+  displayedColumns: string[] = ['no', 'name', 'folio', 'nombre-razon-social', 'fecha', 'acciones'];
+
+  dataSource = [
+    { no: 1, name: 'FOLAB-03J', folio: 'FOL123', nombreRazonSocial: 'Empresa Unic', fecha: '2024-06-04' },
+    { no: 2, name: 'FOLAB-88H', folio: 'FOL124', nombreRazonSocial: 'Empresa Unic', fecha: '2024-06-03' },
+    { no: 3, name: 'FOLAB-23J', folio: 'FOL125', nombreRazonSocial: 'Empresa Unic', fecha: '2024-06-01' }
+  ];
+
+
+  goUploadFile(){
+    this.route.navigate(['/dashboard/upload-files']);
+  }
+
+  openDialog() {
+  this.dialog.open(ConfirmDeleteDialogComponent, {
+    width: '800px',
+  });
+}
 }
