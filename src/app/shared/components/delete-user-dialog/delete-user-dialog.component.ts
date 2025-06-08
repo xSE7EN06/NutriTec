ca7169497a7bf963cdf2954c8bbaf5e15a7e5b04
xSE7EN06 <agao6105@gmail.com>
2025-06-07 18:31:03 -0600

    fix: crear un componente dialog para eliminar un usuario

---
+++ src/app/shared/components/delete-user-dialog/delete-user-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-user-dialog',
+  standalone: false,
+  templateUrl: './delete-user-dialog.component.html',
+  styleUrl: './delete-user-dialog.component.css'
+})
+export class DeleteUserDialogComponent {
+
+}
