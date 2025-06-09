a73bcb177986cc327f86ec7c624b26c3d881b2ec
xSE7EN06 <agao6105@gmail.com>
2025-06-08 20:04:03 -0600

    feat: agregar funcion para abrir un bottom-sheet

--- src/app/dashboard/account/account.component.ts
+++ src/app/dashboard/account/account.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateEditDialogComponent } from '../../shared/components/create-edit-dialog/create-edit-dialog.component';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { BottomSheetPhotoComponent } from '../../shared/components/bottom-sheet-photo/bottom-sheet-photo.component';
 
 @Component({
   selector: 'app-account',
@@ -7,5 +11,35 @@ import { Component } from '@angular/core';
   styleUrl: './account.component.css'
 })
 export class AccountComponent {
+  private bottomSheet = inject(MatBottomSheet);
 
+
+  constructor(private dialog: MatDialog){
+
+  }
+
+  openDialog(modo: 'crear' | 'editar') {
+      const dialogRef = this.dialog.open(CreateEditDialogComponent, {
+        width: '90%',
+        maxWidth: '1000px',
+        minWidth: '900px',
+        maxHeight: '100vh',
+        position: { top: '50px' },
+        panelClass: 'custom-dialog-container',
+        disableClose: false,
+        autoFocus: false,
+        data:{
+          modo: modo
+        }
+      });
+  
+       dialogRef.afterClosed().subscribe(result => {
+        console.log('The patient dialog was closed');
+        // La recarga se maneja a través de la suscripción al servicio
+      });
+    }
+
+   openBottomSheet(): void {
+    this.bottomSheet.open(BottomSheetPhotoComponent);
+  }
 }
