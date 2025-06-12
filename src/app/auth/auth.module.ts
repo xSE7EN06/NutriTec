c12d3f2e8c6a400263b50ef522b7a483f4e9869d
xSE7EN06 <agao6105@gmail.com>
2025-06-11 20:19:52 -0600

    fix: importar SharedModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,6 +6,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { SpinnerLoadingComponent } from '../shared/components/spinner-loading/spinner-loading.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -19,7 +24,11 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatFormField,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
