292a8d5345b31635a1a73f987f429153d07cd538
xSE7EN06 <agao6105@gmail.com>
2025-06-11 22:24:39 -0600

    feat/style: mejorar diseño y crear funciones para cargar archivos xls

--- src/app/dashboard/upload-files/upload-files.component.ts
+++ src/app/dashboard/upload-files/upload-files.component.ts
@@ -11,4 +11,34 @@ export class UploadFilesComponent{
    clienteForm!: FormGroup;
 
   constructor(private fb: FormBuilder) {}
+
+  uploadedFiles: File[] = [];
+  isDragOver = false;
+
+onFileSelected(event: Event): void {
+  const input = event.target as HTMLInputElement;
+  if (input.files) {
+    const files = Array.from(input.files).filter(file => file.name.endsWith('.xlsx'));
+    this.uploadedFiles.push(...files);
+  }
+}
+
+onDragOver(event: DragEvent): void {
+  event.preventDefault();
+  this.isDragOver = true;
+}
+
+onDragLeave(event: DragEvent): void {
+  event.preventDefault();
+  this.isDragOver = false;
+}
+
+onFileDrop(event: DragEvent): void {
+  event.preventDefault();
+  this.isDragOver = false;
+  const files = Array.from(event.dataTransfer?.files || []).filter(file =>
+    file.name.endsWith('.xlsx')
+  );
+  this.uploadedFiles.push(...files);
+}
 }
