1610a77d184538b5feff765da001a860c3aa4786
xSE7EN06 <agao6105@gmail.com>
2025-06-15 19:07:14 -0600

    style(diálogo): actualizar estilos y lógica del diálogo para eliminar archivo

--- src/app/shared/components/delete-user-dialog/delete-user-dialog.component.ts
+++ src/app/shared/components/delete-user-dialog/delete-user-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete-user-dialog',
@@ -8,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class DeleteUserDialogComponent {
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { name: string, action: string},){
+
+  }
+
+
+
+  //funcion para mostrar diferentes mensajes
+  getMessage(): string {
+  if (this.data.action === 'delete-user') {
+    return 'Esta acción eliminará al usuario de forma permanente.';
+  } else if (this.data.action === 'delete-file') {
+    return 'Esto eliminará el archivo que subiste.';
+  } else {
+    return '¿Estás seguro de continuar con esta acción?';
+  }
+}
+  
 }
