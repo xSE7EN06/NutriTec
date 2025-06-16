638c3f418854f252c8a6bf5a1a53ea6cc6021bb5
xSE7EN06 <agao6105@gmail.com>
2025-06-15 19:07:05 -0600

    refactor(diálogo): ajustar lógica del diálogo de confirmación

--- src/app/shared/components/confirm-delete-dialog/confirm-delete-dialog.component.ts
+++ src/app/shared/components/confirm-delete-dialog/confirm-delete-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-confirm-delete-dialog',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './confirm-delete-dialog.component.css'
 })
 export class ConfirmDeleteDialogComponent {
-
+  
 }
